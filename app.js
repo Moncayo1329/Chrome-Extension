394a8f4d6a5c47c0adc33bfee3a24e12d434858e
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-05-08 17:11:58 +0100

    Fist commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let myLeads = []
+let inputEl = document.getElementById("input-el")
+
+let inputBtn = document.getElementById("input-btn") 
+
+inputBtn.addEventListener("click", function( ) {
+
+console.log("Button clicked!")
+
+}) 
+
